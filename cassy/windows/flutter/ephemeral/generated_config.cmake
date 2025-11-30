bf1d210e7338e4a891bed40e1afc2a714b3e7854
Christzyl Ann Casiño <christzylcasino@gmail.com>
2025-12-01 01:21:57 +0800

    Add files via upload

---
+++ cassy/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\xire\\cassy" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\xire\\cassy"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\xire\\cassy\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\xire\\cassy"
+  "FLUTTER_TARGET=C:\\Users\\xire\\cassy\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YjQ1ZmExODk0Ng==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YjU5OTBlNWNjYw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\xire\\cassy\\.dart_tool\\package_config.json"
+)
